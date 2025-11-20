79dfd23932328b9b91b584364e3daf5f83fa9cca
Yudi <yudiyamada10@gmail.com>
2025-11-20 16:03:55 -0300

    feat: add light mode and dark mode

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -2,19 +2,24 @@ import "./header.component.css";
 
 import { BsCart3 } from "react-icons/bs";
 
+import ThemeToggleButton from "../theme-toggle-button/theme.toggle.button.component";
+
 function Header() {
   return (
     <header className="header-container">
-      <h2 className="header-title">CLUB CLOTHING</h2>
+      <h2 className="header-title">YAMADA CLUB CLOTHING</h2>
 
-      <ul className="header-items">
-        <li className="header-item">Explorar</li>
-        <li className="header-item">Login</li>
-        <li className="header-item">Criar</li>
-        <li className="header-item">
-          <BsCart3 size={25} />5
-        </li>
-      </ul>
+      <div className="header-menu">
+        <ul className="header-items">
+          <li className="header-item">Explorar</li>
+          <li className="header-item">Login</li>
+          <li className="header-item">Criar</li>
+          <li className="header-item">
+            <BsCart3 size={25} />5
+          </li>
+        </ul>
+        <ThemeToggleButton />
+      </div>
     </header>
   );
 }
