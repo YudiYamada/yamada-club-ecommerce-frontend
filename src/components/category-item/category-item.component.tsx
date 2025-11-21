f1611071b5d677acfbb544c658dd777a53112afa
Yudi <yudiyamada10@gmail.com>
2025-11-21 17:43:33 -0300

    refactor: add styled components category item component

--- src/components/category-item/category-item.component.tsx
+++ src/components/category-item/category-item.component.tsx
@@ -7,9 +7,7 @@ interface CategoryItemProps {
 
 function CategoryItem({ category }: CategoryItemProps) {
   return (
-    <CategoryItemContainer
-      style={{ backgroundImage: `url(${category.imageUrl})` }}
-    >
+    <CategoryItemContainer backgroundImage={category.imageUrl}>
       <CategoryName>
         <p>{category.displayName}</p>
         <p>Explorar</p>
