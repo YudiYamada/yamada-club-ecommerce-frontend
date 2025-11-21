415e4d3c4a11de514a8e37f95c332fb0a95d34ff
Yudi <yudiyamada10@gmail.com>
2025-11-21 17:07:54 -0300

    feat: add theme colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,19 @@
+const Colors = {
+  primary: "#212529",
+  text: {
+    white: "#f8f9fa",
+    dark: "#343a40",
+  },
+  background: {
+    white: "#f8f9fa",
+    dark: "#212529",
+  },
+  input: {
+    background: "#e9ecef",
+    placeholder: "#6c757d",
+  },
+  error: "#ff6a6a",
+  focus: "#ffbb33",
+};
+
+export default Colors;
