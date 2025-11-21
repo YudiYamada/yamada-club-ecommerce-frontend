6c1d6f332c4bad8b35a446a97e64ec6387f1b5e3
Yudi <yudiyamada10@gmail.com>
2025-11-21 16:36:23 -0300

    feat: add category item component

---
+++ src/components/category-item/category-item.component.tsx
@@ -0,0 +1,23 @@
+import "./category-item.component.css";
+
+import type { Category } from "../../types/category.types";
+
+interface CategoryItemProps {
+  category: Category;
+}
+
+function CategoryItem({ category }: CategoryItemProps) {
+  return (
+    <div
+      className="category-item-container"
+      style={{ backgroundImage: category.imageUrl }}
+    >
+      <div className="category-name">
+        <p>{category.displayName}</p>
+        <p>Explorar</p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
