851ec747e1538d6c269a33378a510dc66458793f
Yudi <yudiyamada10@gmail.com>
2025-11-21 17:37:03 -0300

    refactor: add styled components category item component

--- src/components/category-item/category-item.styles.ts
+++ src/components/category-item/category-item.styles.ts
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
+import Colors from "../../theme/theme.colors";
+
 export const CategoryItemContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 100%;
-  background-color: var(--category-item-background);
   grid-gap: 15px;
   background-size: cover;
   background-repeat: no-repeat;
@@ -18,7 +19,7 @@ export const CategoryItemContainer = styled.div`
 `;
 
 export const CategoryName = styled.div`
-  color: var(--text);
+  color: ${Colors.text.white};
   text-align: center;
   background: rgba(233, 236, 239, 0.45);
   padding-top: 10px;
@@ -26,7 +27,7 @@ export const CategoryName = styled.div`
   padding-right: 30px;
   padding-left: 30px;
   border-radius: 10px;
-  border: 1px solid var(--background);
+  border: 1px solid ${Colors.background.dark};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   transition: all 0.5s ease;
 
@@ -35,7 +36,7 @@ export const CategoryName = styled.div`
     background: rgba(233, 236, 239, 0.55);
   }
 
-  &:nth-child(1) {
+  & p:nth-child(1) {
     font-weight: 600;
   }
 `;
