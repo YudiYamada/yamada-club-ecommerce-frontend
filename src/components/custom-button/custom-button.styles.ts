843598cd1a30fe6e11ecc1ffde2a8fe70f819821
Yudi <yudiyamada10@gmail.com>
2025-11-24 16:15:14 -0300

    feat: add custom button component

---
+++ src/components/custom-button/custom-button.styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+import Colors from "../../theme/theme.colors";
+
+export const CustomButtonContainer = styled.button`
+  width: 100%;
+  background-color: ${Colors.background.dark};
+  color: ${Colors.text.white};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
+  border: none;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-right: 15px;
+  padding-left: 15px;
+  font-weight: 600;
+  transition: all 0.5s ease;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #000000;
+    color: ${Colors.focus};
+  }
+`;
+
+export const IconContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  margin-right: 8px;
+`;
