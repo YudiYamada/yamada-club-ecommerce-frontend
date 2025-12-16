19f9289554cd9b2e186c138ebb8166b1e57a3030
Yudi <yudiyamada10@gmail.com>
2025-12-16 15:15:13 -0300

    feat: add header to signup page

--- src/pages/sign-up/sign-up.page.tsx
+++ src/pages/sign-up/sign-up.page.tsx
@@ -4,6 +4,7 @@ import validator from "validator";
 
 import CustomButton from "../../components/custom-button/custom-button.component";
 import CustomInput from "../../components/custom-input/custom-input.component";
+import Header from "../../components/header/header.component";
 import InputErrorMessage from "../../components/input-error-message/input-error-message.component";
 import {
   SignUpContainer,
@@ -38,6 +39,7 @@ function SignUpPage() {
 
   return (
     <>
+      <Header />
       <SignUpContainer>
         <SignUpContent>
           <SignUpHeadline>Crie sua conta</SignUpHeadline>
