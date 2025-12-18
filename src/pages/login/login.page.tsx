f2f5fe636e060bb19fcd3f2068c88a091470b0e2
Yudi <yudiyamada10@gmail.com>
2025-12-18 17:34:40 -0300

    feat: add login

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,3 +1,4 @@
+import { type AuthError, signInWithEmailAndPassword } from "firebase/auth";
 import { useForm } from "react-hook-form";
 import { BsGoogle } from "react-icons/bs";
 import { FiLogIn } from "react-icons/fi";
@@ -7,6 +8,7 @@ import CustomButton from "../../components/custom-button/custom-button.component
 import CustomInput from "../../components/custom-input/custom-input.component";
 import Header from "../../components/header/header.component";
 import InputErrorMessage from "../../components/input-error-message/input-error-message.component";
+import { auth } from "../../config/firebase.config";
 import {
   LoginContainer,
   LoginContent,
@@ -23,15 +25,38 @@ interface LoginForm {
 function LoginPage() {
   const {
     register,
-    formState: { errors },
     handleSubmit,
+    setError,
+    formState: { errors },
   } = useForm<LoginForm>();
 
-  const handleSubmitPress = (data: LoginForm) => {
-    console.log(data);
-  };
+  const handleSubmitPress = async (data: LoginForm) => {
+    // console.log(data);
+    // error de senha Firebase: Error (auth/invalid-credential).
+    // at createErrorInternal (firebase_auth.js?v=ae413fed:698:37)
+    // at _fail (firebase_auth.js?v=ae413fed:660:9)
+    // at _performFetchWithErrorHandling (firebase_auth.js?v=ae413fed:1138:9)
+    // at async _performSignInRequest (firebase_auth.js?v=ae413fed:1149:26)
+    // at async _signInWithCredential (firebase_auth.js?v=ae413fed:4631:20)
+    // at async handleSubmitPress
+    try {
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password,
+      );
 
-  console.log(errors);
+      console.log(userCredentials);
+    } catch (error) {
+      const _error = error as AuthError;
+
+      if (_error.code === "auth/invalid-credential") {
+        setError("email", { type: "invalid" });
+        setError("password", { type: "invalid" });
+        return;
+      }
+    }
+  };
 
   return (
     <>
@@ -62,7 +87,8 @@ function LoginPage() {
             {errors?.email?.type === "required" && (
               <InputErrorMessage>O e-mail é obrigatório.</InputErrorMessage>
             )}
-            {errors?.email?.type === "validate" && (
+            {(errors?.email?.type === "validate" ||
+              errors?.email?.type === "invalid") && (
               <InputErrorMessage>O e-mail é inválido.</InputErrorMessage>
             )}
           </LoginInputContainer>
@@ -76,6 +102,9 @@ function LoginPage() {
             />
             {errors?.password?.type === "required" && (
               <InputErrorMessage>A senha é obrigatória.</InputErrorMessage>
+            )}
+            {errors?.password?.type === "invalid" && (
+              <InputErrorMessage>A senha é inválida.</InputErrorMessage>
             )}
           </LoginInputContainer>
 
