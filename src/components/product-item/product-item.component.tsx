a4753cc99041ae4030125d83f972e2ea75e0e383
Yudi <yudiyamada10@gmail.com>
2025-12-30 16:32:38 -0300

    feat: add product item component

---
+++ src/components/product-item/product-item.component.tsx
@@ -0,0 +1,27 @@
+import { type FunctionComponent } from "react";
+
+import { type Product } from "../../types/product.types";
+import {
+  ProductContainer,
+  ProductImage,
+  ProductInfo,
+} from "./product-item.styles";
+
+interface ProductItemProps {
+  product: Product;
+}
+
+const ProductItem: FunctionComponent<ProductItemProps> = ({ product }) => {
+  return (
+    <ProductContainer>
+      <ProductImage imageUrl={product.imageUrl} />
+
+      <ProductInfo>
+        <p>{product.name}</p>
+        <p>R${product.price}</p>
+      </ProductInfo>
+    </ProductContainer>
+  );
+};
+
+export default ProductItem;
