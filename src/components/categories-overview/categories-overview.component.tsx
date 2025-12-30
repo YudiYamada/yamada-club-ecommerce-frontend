2d0c8fff141a24aaaf0f5060af6c7fdf44e46f8b
Yudi <yudiyamada10@gmail.com>
2025-12-30 16:31:57 -0300

    feat: add categories overview component

---
+++ src/components/categories-overview/categories-overview.component.tsx
@@ -0,0 +1,29 @@
+import { type FunctionComponent, useContext, useEffect } from "react";
+
+import { CategoryContext } from "../../contexts/category.context";
+import CategoryOverview from "../category-overview/category-overview.component";
+import Loading from "../loading/loading.component";
+import { Container } from "./categories-overview.styles.component";
+
+const CategoriesOverview: FunctionComponent = () => {
+  const { categories, isLoading, fetchCategories } =
+    useContext(CategoryContext);
+
+  useEffect(() => {
+    if (categories.length === 0) {
+      fetchCategories();
+    }
+  }, []);
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <Container>
+      {categories.map((category) => (
+        <CategoryOverview key={category.id} category={category} />
+      ))}
+    </Container>
+  );
+};
+
+export default CategoriesOverview;
