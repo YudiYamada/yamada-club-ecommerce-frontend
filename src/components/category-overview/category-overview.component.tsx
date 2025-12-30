cdc9e6afe6017581cb610c99138438d7a7bc1ee4
Yudi <yudiyamada10@gmail.com>
2025-12-30 16:32:23 -0300

    feat: add category overviwe component

---
+++ src/components/category-overview/category-overview.component.tsx
@@ -0,0 +1,31 @@
+import { type FunctionComponent } from "react";
+
+import { type Category } from "../../types/category.types";
+import ProductItem from "../product-item/product-item.component";
+import {
+  CategoryContainer,
+  CategoryTitle,
+  ProductsContainer,
+} from "./category-overview.styles";
+
+interface CategoryOverviewProps {
+  category: Category;
+}
+
+const CategoryOverview: FunctionComponent<CategoryOverviewProps> = ({
+  category,
+}) => {
+  return (
+    <CategoryContainer>
+      <CategoryTitle>{category.displayName}</CategoryTitle>
+
+      <ProductsContainer>
+        {category.products.slice(0, 4).map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </ProductsContainer>
+    </CategoryContainer>
+  );
+};
+
+export default CategoryOverview;
